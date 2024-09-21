859e46d6dee977bcd713dfb7d565e43dc0f92201
pratham-darooka <pdarooka1011@gmail.com>
2024-09-21 23:17:56 +0530

    fixed width

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -25,7 +25,7 @@ export function BlogPosts() {
               <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
                 {post.metadata.title}
               </p>
-              <p className="text-neutral-600 dark:text-neutral-400 w-[200px] tabular-nums">
+              <p className="text-neutral-600 dark:text-neutral-400 w-[300px] tabular-nums">
                 {post.metadata.domain},&nbsp;
                 {formatDate(post.metadata.publishedAt, false)}
               </p>
