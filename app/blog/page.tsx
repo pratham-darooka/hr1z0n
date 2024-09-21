a8c137e9d02f2d2f6311ed42f8f6fa433ccc195f
pratham-darooka <pdarooka1011@gmail.com>
2024-09-21 09:35:18 +0530

    message change

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -8,7 +8,7 @@ export const metadata = {
 export default function Page() {
   return (
     <section>
-      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">behold, my thoughts and experiences...</h1>
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">welcome to my brain</h1>
       <BlogPosts />
     </section>
   )
