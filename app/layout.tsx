4dc4623baad66989c5873424d5212083ee1bb865
pratham-darooka <pdarooka1011@gmail.com>
2024-09-21 14:07:56 +0530

    removed analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,7 @@ import type { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
 import { Navbar } from './components/nav'
-import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from '@vercel/speed-insights/next'
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import Footer from './components/footer'
 import { baseUrl } from './sitemap'
 import Head from 'next/head';
@@ -64,7 +63,6 @@ export default function RootLayout({
           <Navbar />
           {children}
           <Footer />
-          <Analytics />
           <SpeedInsights />
         </main>
       </body>
