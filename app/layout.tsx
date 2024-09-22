cad757c53b4e9e0c23c4da781b6334d3cd260317
pratham-darooka <pdarooka1011@gmail.com>
2024-09-22 20:45:35 +0530

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
 import { Navbar } from './components/nav'
+import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import Footer from './components/footer'
 import { baseUrl } from './sitemap'
@@ -63,6 +64,7 @@ export default function RootLayout({
           <Navbar />
           {children}
           <Footer />
+          <Analytics />
           <SpeedInsights />
         </main>
       </body>
